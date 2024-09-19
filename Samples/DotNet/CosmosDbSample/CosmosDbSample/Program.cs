0fec14803b722ceb51bbdbf8dfb6c1be0c4ed1f8
jgao <aus.jacy@gmail.com>
2024-09-19 20:34:32 +1000

    WIP - Cosmosdb sample

---
+++ Samples/DotNet/CosmosDbSample/CosmosDbSample/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.Cosmos;
+using System.ComponentModel;
+using System.Net;
+
+internal class Program
+{
+    private static readonly string databaseId = "samples";
+    private static readonly string containerId = "container-samples";
+
+    private static Database database = null;
+
+    public static async Task Main(string[] args)
+    {
+        CosmosClient client = new CosmosClient(endpoint, authKey);
+
+        database = await client.CreateDatabaseIfNotExistsAsync(databaseId);
+
+    }
+
+    // <CreateContainer>
+    private static async Task<Container> CreateContainer()
+    {
+        // Set throughput to the minimum value of 400 RU/s
+        ContainerResponse simpleContainer = await database.CreateContainerIfNotExistsAsync(
+            id: containerId,
+            partitionKeyPath: partitionKeyPath,
+            throughput: 400);
+
+        Console.WriteLine($"{Environment.NewLine}1.1. Created container :{simpleContainer.Container.Id}");
+        return simpleContainer;
+    }
+    // </CreateContainer>
+}
