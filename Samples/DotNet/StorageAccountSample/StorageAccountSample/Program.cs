547fde02b37e0ace4f33154a6a90a1d62a4a491f
jgao <aus.jacy@gmail.com>
2024-10-03 19:27:25 +1000

    added more storage account ops

--- Samples/DotNet/StorageAccountSample/StorageAccountSample/Program.cs
+++ Samples/DotNet/StorageAccountSample/StorageAccountSample/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Azure.Identity;
 using Azure.Storage;
 using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System.Reflection.Metadata;
 
 internal class Program
 {
@@ -9,6 +11,8 @@ internal class Program
     private static readonly string accountName = "devstoreaccount1";
 
     private static readonly string accountKey = "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==";
+
+    private static readonly string blobName = "TestBlob";
 
     public static async Task Main(string[] args)
     {
@@ -19,5 +23,25 @@ internal class Program
 
         BlobContainerClient bcc = await client.CreateBlobContainerAsync("sample-container");
 
+        // Upload Blob
+        BlobClient blobClient = bcc.GetBlobClient(blobName);
+        string blobContents = "Sample blob data";
+
+        await blobClient.UploadAsync(BinaryData.FromString(blobContents), overwrite: true);
+
+        Console.WriteLine($"A blob with context {blobContents} has been uploaded");
+
+        // Download Blob
+        BlobDownloadResult downloadResult = await blobClient.DownloadContentAsync();
+        
+        string downloadedBlobContents = downloadResult.Content.ToString();
+
+        Console.WriteLine($"A blob with context {downloadedBlobContents} has been downloaded");
+
+        // Delete Blob
+        await blobClient.DeleteAsync();
+
+        Console.WriteLine($"A blob has been deleted");
     }
+
 }
