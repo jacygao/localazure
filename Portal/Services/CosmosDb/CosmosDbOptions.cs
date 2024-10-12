ddaaaa0ba3b2918dfc1e19bd117dbaa102a6fedf
jacygao <aus.jacy@gmail.com>
2024-10-12 14:38:01 +0800

    WIP - portal

---
+++ Portal/Services/CosmosDb/CosmosDbOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Portal.Services.CosmosDb
+{
+    public class CosmosDbOptions
+    {
+        public string Endpoint { get; set; }
+
+        public string AuthKey { get; set; }
+    }
+}
---
+++ Portal/Services/CosmosDb/CosmosDbService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.Cosmos;
+
+namespace Portal.Services.CosmosDb
+{
+    public class CosmosDbService
+    {
+        private readonly CosmosDbOptions _options;
+        private readonly CosmosClient _client;
+
+        public CosmosDbService(CosmosDbOptions options) { 
+            _options = options;
+            _client = new CosmosClient(options.Endpoint, options.AuthKey);
+        }
+
+        public async Task CreateDatabaseAsync()
+        {
+
+        }
+
+        public async Task CreateContainerAsync()
+        {
+
+        }
+
+        public async Task CreateItemAsync()
+        {
+
+        }
+
+        public async Task UpdateItem()
+        {
+
+        }
+
+        public async Task DeleteItem()
+        {
+
+        }
+    }
+}
