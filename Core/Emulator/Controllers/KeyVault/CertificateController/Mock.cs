a3922a5da6747a92598b19861c7f2d4c97e75e3d
jgao <aus.jacy@gmail.com>
2024-11-09 00:09:46 +1100

    added mock response for certificate get and create

---
+++ Core/Emulator/Controllers/KeyVault/CertificateController/Mock.cs
@@ -0,0 +1,11 @@
+﻿using CertificateController;
+
+namespace Emulator.Controllers.KeyVault.CertificateController
+{
+    public class Mock
+    {
+        public CertificateBundle? GetCertificateAsync { get; set; }
+
+        public CertificateOperation? CreateCertificateAsync {  get; set; }
+    }
+}
