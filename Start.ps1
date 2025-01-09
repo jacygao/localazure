e10a8468b094b4fc9d0f82541ef83be2bd71baa0
jgao <aus.jacy@gmail.com>
2025-01-09 11:33:43 +1100

    updated script to run with dotnet

--- Start.ps1
+++ Start.ps1
@@ -1,1 +1,17 @@
-./Scripts/Core/ValidateEnv.ps1
+#!/usr/bin/env pwsh
+
+# Validate the environment
+try {
+    ./Scripts/Core/ValidateEnv.ps1
+} catch {
+    Write-Host "Environment validation failed."
+    exit 1
+}
+
+Start the application
+try {
+    dotnet run --project ./Gateway/Gateway.AppHost/Gateway.AppHost.csproj
+} catch {
+    Write-Host "Failed to start the application."
+    exit 1
+}
